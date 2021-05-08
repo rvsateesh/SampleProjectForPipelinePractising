d654660a13aa7c3af393d63e80e57e0ec6b0a22a
rvsateesh <rvsateesh@gmail.com>
2021-05-08 11:44:44 +0530

    second commit

---
+++ src/test/java/com/testing/samplecases/TestClass1.java
@@ -0,0 +1,11 @@
+package com.testing.samplecases;
+
+import org.testng.annotations.Test;
+
+@Test
+public class TestClass1 {
+
+	public void method1() {
+		System.out.println("Welcome to test case 1");
+	}
+}
